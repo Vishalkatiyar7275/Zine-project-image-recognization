1ac5014b0de2a6830433cd277267c05cd3c92b5e
Vishalkatiyar7275 <127015462+Vishalkatiyar7275@users.noreply.github.com>
2023-04-22 11:00:26 +0530

    Update final_code_for_object_detection.py

--- final_code_for_object_detection.py
+++ final_code_for_object_detection.py
@@ -1,6 +1,6 @@
 
 
-import cv2
+import cv2 as cv
 import numpy as np
 
 # define color ranges for the shapes
@@ -10,16 +10,16 @@ colors = {
     'blue': [(110, 50, 50), (150, 255, 255)]
 }
 
-# define shape detection functionimport cv2
+# define shape detection function import cv2
 
 
 def detect_shape(cnt):
-    perimeter = cv2.arcLength(cnt, True)
-    approx = cv2.approxPolyDP(cnt, 0.04 * perimeter, True)
+    perimeter = cv.arcLength(cnt, True)
+    approx = cv.approxPolyDP(cnt, 0.04 * perimeter, True)
     if len(approx) == 3:
         return "Triangle"
     elif len(approx) == 4:
-        x,y,w,h = cv2.boundingRect(approx)
+        x,y,w,h = cv.boundingRect(approx)
         aspect_ratio = float(w)/h
         if aspect_ratio >= 0.95 and aspect_ratio <= 1.05:
             return "Square"
@@ -30,38 +30,38 @@ def detect_shape(cnt):
         return None
 
 # define video source
-cap = cv2.VideoCapture(0)
+cap = cv.VideoCapture(0)
 
 while True:
     # read frame from video source
     ret, frame = cap.read()
 
     # convert frame to HSV color space
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
 
     # loop over color ranges and find contours of shapes
     for color in colors:
         lower, upper = colors[color]
-        mask = cv2.inRange(hsv, np.array(lower), np.array(upper))
-        contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        mask = cv.inRange(hsv, np.array(lower), np.array(upper))
+        contours, hierarchy = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
         # loop over contours and detect shapes
         for cnt in contours:
             shape = detect_shape(cnt)
             if shape:
                 # draw bounding box around shape
-                x, y, w, h = cv2.boundingRect(cnt)
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                x, y, w, h = cv.boundingRect(cnt)
+                cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
                 # print shape and color to terminal
                 print("Detected {} {} object".format(color, shape))
 
     # show frame with detected shapes
-    cv2.imshow('frame', frame)
+    cv.imshow('frame', frame)
 
     # exit program on 'q' key press
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
 # release video source and destroy all windows
 cap.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
